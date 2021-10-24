3d50d07135bf0638e77ea7d402463854187d4c74
Liz-Conway <zil.conway@gmail.com>
2021-10-24 12:02:00 +0100

    FEAT: Add pause between typed sentences

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,5 @@
 function playAnimation() {
 	let pictureImage = document.getElementsByClassName("storyPicture")[0];
-	let textParagraphs = document.getElementsByTagName("p");
-	console.log(textParagraphs);
-	console.log("First:  ", textParagraphs[0]);
-	console.log("TEXT:  ", textParagraphs[0].innerHTML);
 	
 	pictureImage.src = "assets/images/spinning-pandemic-globe-large.gif";
 	
@@ -11,19 +7,31 @@ function playAnimation() {
 						"A pandemic is where many people in a large area become sick.", 
 						"A pandemic is usually caused by a virus."];
 						
-	let sceneOneParagraph = "This story is about pandemics and the Coronavirus.  A pandemic is where many people in a large area become sick.  A pandemic is usually caused by a virus.";
+	/* typed.js allows you to pause a sentence
+		by inserting a '^' symbol followed by the number of milliseconds to pause
+		E.G. ^500 pauses the typing for a half a second (500ms)
+		 */
+	let sentencePause = "^1000";
+	let sceneOneParagraph = "This story is about pandemics and the Coronavirus.  ";
+	sceneOneParagraph += sentencePause;
+	sceneOneParagraph += "A pandemic is where many people in a large area become sick.  ";
+	sceneOneParagraph += sentencePause;
+	sceneOneParagraph += "A pandemic is usually caused by a virus.";
 	
 	clearParagraph();
 	typeParagraph(sceneOneParagraph);
 	
-	/*for(let i = 0; i < sceneOneText.length; i++) {
-		console.log(textParagraphs);
-		textParagraphs[i].innerHTML = sceneOneText[i];
-	}*/
 }
 
+/** Use typed.js to type out the entered text */
 function typeParagraph($text) {
-	var typed = new Typed('.para1', {
+	/* The first argument is the class of the elment where the text will be typed 
+		strings = The text to type
+		typeSpeed = how fast the text types
+		loop = whether the text is typed once (false), or over and over again (true)
+		showCursor = whether to show a typing cursor as text is typed
+	*/
+	var typed = new Typed('.storyParagraph', {
 		strings: [$text],
 		typeSpeed: 100,
 		loop: false,
@@ -31,8 +39,11 @@ function typeParagraph($text) {
 	});
 }
 
+/** Clears the text in the storyParagraph
+	Otherwise the typing for the next scene will not work
+ */
 function clearParagraph() {
-	let storyParagraph = document.getElementsByClassName("para1")[0];
+	let storyParagraph = document.getElementsByClassName("storyParagraph")[0];
 	
 	storyParagraph.innerHTML = "";
 }
