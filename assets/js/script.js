9a8b22ccc71bc1507658a016b1acf569a6016f44
Liz-Conway <zil.conway@gmail.com>
2021-11-01 11:23:41 +0000

    FEAT: Play sound for each scene

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,16 @@ function playAnimation() {
 						"A pandemic is where many people in a large area become sick.  ", 
 						"A pandemic is usually caused by a virus."];
 	
+	/* https://www.delftstack.com/howto/javascript/play-audio-javascript/ */
+	let sceneOneAudio = new Audio("assets/audio/scene1.mp3");
+	
 	let sceneTwoText = ["The Coronavirus is a virus that is spreading fast and causing a worldwide pandemic now.<br><br>", 
 						"Viruses are so small that it takes an electron microscope to see them.  ", 
 						"People can't see if a virus is near them."];
 	
 	let sceneTwoPic = "assets/images/grumpy-spike.gif";
+	
+	let sceneTwoAudio = new Audio("assets/audio/scene2.mp3");
 	
 	let sceneThreePic = "assets/images/light-bulb.gif";
 	
@@ -20,50 +25,55 @@ function playAnimation() {
 						"They use healthy habits and work together to make it harder for the Coronavirus to spread.  ",
 						"This helps keep people healthy during the pandemic."];
 	
+	let sceneThreeAudio = new Audio("assets/audio/scene3.mp3");
+	
 	let sceneFourPic = "assets/images/wash-hands.gif";
 	
 	let sceneFourText = ["People wash their hands well and often during a pandemic.  ", 
 						"Adults make sure kids know how to wash their hands well.  ", 
 						"And, adults remind kids to wash their hands a lot."];
 	
-	let sceneOne = [sceneOnePic, sceneOneText];
+	let sceneFourAudio = new Audio("assets/audio/scene4.mp3");
 	
-	let sceneTwo = [sceneTwoPic, sceneTwoText];
+	let sceneOne = [sceneOnePic, sceneOneText, sceneOneAudio];
 	
-	let sceneThree = [sceneThreePic, sceneThreeText];
+	let sceneTwo = [sceneTwoPic, sceneTwoText, sceneTwoAudio];
 	
-	let sceneFour = [sceneFourPic, sceneFourText];
+	let sceneThree = [sceneThreePic, sceneThreeText, sceneThreeAudio];
+	
+	let sceneFour = [sceneFourPic, sceneFourText, sceneFourAudio];
 	
 	let scenes = [sceneOne, sceneTwo, sceneThree, sceneFour];
 						
-	/* typed.js allows you to pause a sentence
-		by inserting a '^' symbol followed by the number of milliseconds to pause
-		E.G. ^500 pauses the typing for a half a second (500ms)
-	 */
-	let sentencePause = "^1000";
-	
-	showScenes(scenes, pictureContainer, sentencePause);
+	showScenes(scenes, pictureContainer);
 	
 }
 
-function showScenes(scenes, pictureContainer, sentencePause) {
-	let sceneTime = 30000;
+function showScenes(scenes, pictureContainer) {
+	let sceneTime = 35000;
 	
 	for(let i = 0; i < scenes.length; i++) {
 		let time = sceneTime * i;
 		let picture = scenes[i][0];
 		let text = scenes[i][1];
+		let audio = scenes[i][2];
 		
 		if(i===0) {
 			time = 0;
 		}
 		/** https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing2 */
 		/** https://www.programiz.com/javascript/examples/pass-parameter-setTimeout */
-		setTimeout(showScene, time, pictureContainer, picture, text, sentencePause);
+		setTimeout(showScene, time, pictureContainer, picture, text, audio);
 	}
 }
 
-function showScene(pictureImage, picture, sentences, sentencePause) {
+function showScene(pictureImage, picture, sentences, audio) {
+	/* typed.js allows you to pause a sentence
+		by inserting a '^' symbol followed by the number of milliseconds to pause
+		E.G. ^500 pauses the typing for a half a second (500ms)
+	 */
+	let sentencePause = "^1000";
+	
 	pictureImage.src = picture;
 	
 	let sceneParagraph = "";
@@ -76,7 +86,7 @@ function showScene(pictureImage, picture, sentences, sentencePause) {
 	
 	clearParagraph();
 	typeParagraph(sceneParagraph);
-	
+	audio.play();
 }
 
 
